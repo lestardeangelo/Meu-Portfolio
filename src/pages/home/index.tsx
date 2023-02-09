97983a0a75d56aa3f59e73f3ec00082153e1f28e
LestardeAngelo <lestarherminio@gmail.com>
2023-02-08 23:01:44 -0300

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -52,7 +52,7 @@ export const Home = (): JSX.Element => {
               </Text>{" "}
             </Text>
             <Text type="body1" color="grey2">
-            Meu interesse pela tecnologia começou aos 12 anos, onde comecei a estudar designer e sistemas. 
+            Meu interesse pela tecnologia começou aos 12 anos, onde comecei a estudar design, e sistemas. 
             Iniciei minha carreira em áreas administrativas, onde sempre tive a liberdade de apresentar algo a mais com o que sabia,
             e isso me motivou a buscar algo novo para me aprimorar. 
             Aos 16 anos tive interesse na área de programação, e logo dei início aos estudos.
